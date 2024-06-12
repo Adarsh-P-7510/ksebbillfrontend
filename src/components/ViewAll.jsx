3fdfd04a6600eee0c9aacac3f7ee49ca8a1b694c
Adarsh-P-7510 <adarshp7510@gmail.com>
2024-06-13 00:53:36 +0530

    created

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-const 
- = () => {
-  return (
+
+    const ViewAll = () => {
+        const [data,setdata] = useState([])
+        const fetchdata=()=>{
+            axios.get("http://localhost:8080/ViewAll").then(
+                (response)=>{
+                    setdata(response.data)
+                    
+                }
+            ).catch().finally()
+        }
     
-  )
+    
+           useEffect(()=>{fetchdata()},[])
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">consumernumber</th>
+                                    <th scope="col">name</th>
+                                    <th scope="col">email</th>
+                                    <th scope="col">mobile</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map(
+                                        (value, index) => {
+                                            return <tr>
+                                                <th scope="row">{value.consumernumber}</th>
+                                                <td>{value.name}</td>
+                                                <td>{value.email}</td>
+                                                <td>{value.mobile}</td>
+                                            </tr>
+                                        }
+                                    )
+                                }
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default 
+export default ViewAll
