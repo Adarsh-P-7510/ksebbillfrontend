e9b35242025b012c071d2b86e124ed66cb10b4f5
Adarsh-P-7510 <adarshp7510@gmail.com>
2024-06-13 01:39:52 +0530

    created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Adduser from './components/Adduser';
 import ViewAll from './components/ViewAll';
+import SearchUser from './components/SearchUser';
 
 function App() {
   return (
     <div>
       <Adduser/>
       <ViewAll/>
+      <SearchUser/>
+      
       
     </div>
   );
